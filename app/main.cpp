710b5ecbf8cceb57f1bb2a60a3ff6bcbbb2862b2
Julen <juliantxo14@gmail.com>
2026-07-27 00:05:56 +0200

    Add Xake application branding

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +9,8 @@ int main(int argc, char *argv[])
     QApplication::setOrganizationName(QStringLiteral("Xake"));
     QApplication::setApplicationName(QStringLiteral("Xake"));
     QApplication::setApplicationDisplayName(QStringLiteral("Xake"));
+    QApplication::setWindowIcon(
+        QIcon(QStringLiteral(":/assets/branding/xake-logo.png")));
     MainWindow w;
     w.show();
     return a.exec();
